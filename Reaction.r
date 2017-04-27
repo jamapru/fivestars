1b4103fd5094ff6d21db9ce40fea32f70949e3c0
Jaylyn Pruitt <jamapru@users.noreply.github.com>
2017-04-27 03:21:40 -0400

    Update and rename Reaction.r to Full.Reaction

--- Reaction.r
+++
@@ -1,6 +0,0 @@
-#placeholder for reaction function
-#gives output rating on each scale of emotions using the Bayesian sentiment analysis scale
-reaction <- function(r)
-  {
-    reaction <- classify_emotion(New_Patio_Reviews, algorithm="bases", prior=1.0)
-  }
